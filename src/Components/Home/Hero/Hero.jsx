70915aed10ad91c58c45669472e265a419377bb8
justinait <justiiturraspe@gmail.com>
2024-08-23 17:18:36 -0300

    home

--- src/Components/Home/Hero/Hero.jsx
+++ src/Components/Home/Hero/Hero.jsx
@@ -5,8 +5,11 @@ import './Hero.css'
 function Hero() {
   return (
     <div>
-        <img src={hero} alt="" className='hero'/>
-        <p>Asociación Mutual Dos de Agosto: Tu apoyo, nuestro compromiso.</p>
+        {/* <img src={hero} alt=""/> */}
+        <div className='hero'>
+            <p>Tu apoyo, nuestro compromiso</p>
+            <p>Asociación Mutual Dos de Agosto</p>
+        </div>
         <p className='ourBenefitsHero'>Conocé nuestros <br /> <strong>BENEFICIOS</strong></p>
     </div>
   )
