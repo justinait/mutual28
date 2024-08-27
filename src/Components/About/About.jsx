1baa610f9278998909c4446ec9702bf353ef8c3b
justinait <justiiturraspe@gmail.com>
2024-08-27 12:01:28 -0300

    about contenido

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -3,7 +3,87 @@ import React from 'react'
 function About() {
   return (
     <div>
-      <h2>            Acerca de        </h2>
+      <div>
+        <h2>¿Quiénes somos?</h2>
+        <p>
+          En la Asociación Mutual Dos de Agosto, somos una comunidad comprometida con el bienestar y el desarrollo de nuestros socios. 
+          Nacimos con la misión de brindar soluciones integrales, ofreciendo beneficios exclusivos y servicios personalizados que se adaptan 
+          a las necesidades de cada miembro. Nos destacamos por nuestra atención cercana y el apoyo constante que brindamos, siempre con el 
+          objetivo de mejorar la calidad de vida de quienes confían en nosotros.
+          <br /><br />
+          Formamos una red sólida y confiable, donde cada socio es parte fundamental de nuestra gran familia. 
+          En la Asociación Mutual Dos de Agosto, creemos en el poder de la solidaridad y en la fuerza del trabajo en conjunto para construir 
+          un futuro más seguro y próspero para todos.
+
+        </p>
+      </div>
+
+      <div>
+        <h2>Nuestras Sedes</h2>
+        <div>
+          <h4>Delegación Central</h4>
+          <p>
+            <strong>Dirección:</strong> <br /> Av. Rivadavia 1424 – Piso 6º – C.A.B.A. <br />
+            <strong>Teléfono:</strong> <br /> +54 9 11 7181-9952
+          </p>
+        </div>
+        <div>
+          <h4>San Juan</h4>
+          <p>
+            <strong>Delegado: </strong> <br /> Marcelo Rodríguez
+          </p>
+          <p> <strong>Integrantes</strong> <br /> Edgardo Rodríguez <br />          Gemma Zavalla</p>
+        </div>
+        <div>
+          <h4>Jujuy</h4>
+          <p>
+            <strong>Delegado: </strong> <br /> Marcelo Rodríguez
+          </p>
+          <p> <strong>Integrantes</strong> <br /> Edgardo Rodríguez <br />          Gemma Zavalla</p>
+        </div>
+        
+        <div>
+          <h4>Posadas-Misiones</h4>
+          <p>
+            <strong>Delegado: </strong> <br /> Leonardo Lovelli
+          </p>
+        </div>
+
+      </div>
+
+      <div>
+        <h2>Reseñas</h2>
+        <div>
+          <h5>Carlos H. Castro</h5>
+          <p>"No te quedes afuera de vivir tus sueños de la mano de nuestra mutual."</p>
+        </div>
+        <div>
+          <h5>Andre T. O´Connor.</h5>
+          <p>"Afianzados en los principios mutualistas y trabajando constantemente para aplicar los valores que nos rigen, 
+            conformamos un sólido compromiso para con nuestros asociados, la comunidad en la que estamos insertos y las economías regionales."</p>
+        </div>
+      </div>
+
+      <div>
+        <h2>Consejo Directivo</h2>
+        <ul>
+          <li>Presidente: Carlos Horacio Castro</li>
+          <li>Secretario: Leonardo Lovelli</li>
+          <li>Tesorero: Marcelo Rodríguez</li>
+          <li>Vocal Titular: Edgardo Rodríguez</li>
+          <li>Vocal Titular: María Eda Peñafort Sales</li>
+          <li>Vocal Titular: Vicente Costa</li>
+          <li>Vocal Titular Suplente: Bernardo P. Iturraspe</li>
+        </ul>
+
+        <h2>Junta fiscalizadora</h2>
+        <ul>
+          <li>Fiscalizadora: Gema Zavalla</li>
+          <li>Fiscalizador: Luis Alberto Viana</li>
+          <li>Fiscalizador: Andrés Tomas O´Connor</li>
+          <li>Fiscalizador Suplente: Juan José Martincic</li>
+        </ul>
+      </div>
     </div>
   )
 }
